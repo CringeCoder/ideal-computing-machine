7232df4d951848917796a85a18ffb58018129e50
Kyle J. Tomczik <ktomczik@ltu.edu>
2019-03-02 19:38:06 -0500

    First commit

---
+++ app/src/main/java/com/example/parentdisplay/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.parentdisplay;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Setting up a button
+        Button btn = findViewById(R.id.buttonStart);
+
+
+
+
+
+
+
+
+
+    }
+
+}
